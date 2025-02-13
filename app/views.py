1f2abf1d44401d107815d03f0c6b6cf825f5b71d
sanchzj <79599357+sanchzj@users.noreply.github.com>
2025-02-13 13:30:01 -0300

    Update views.py
    Signed-off-by: sanchzj <79599357+sanchzj@users.noreply.github.com>

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index_page(request):
 
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
-     images = services.getAllImages() # Jeremias: Esta función define a 'images' con la lista de todas las cards
+    images = services.getAllImages() # Jeremias: Esta función define a 'images' con la lista de todas las cards
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
